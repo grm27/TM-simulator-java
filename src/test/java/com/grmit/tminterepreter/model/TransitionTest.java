3b9d39ac55961ce5beffc8124556c6514687f369
gmarini <gabriel.marini@lastminute.com>
2024-04-20 19:29:33 +0200

    tested steady state transition (no head movement)

--- src/test/java/com/grmit/tminterepreter/model/TransitionTest.java
+++ src/test/java/com/grmit/tminterepreter/model/TransitionTest.java
@@ -44,6 +44,25 @@ class TransitionTest {
         assertConfigurationEquals(to, transition.apply(from));
     }
 
+    @Test
+    void inBoundarySteadyStateTransition() {
+        ComputationConfig from = new ComputationConfig(
+                0,
+                0,
+                new char[]{'a', 'a'}
+        );
+        Transition transition = new Transition(
+                0, 1, 'a', 'b', 'S'
+        );
+        ComputationConfig to = new ComputationConfig(
+                1,
+                0,
+                new char[]{'b', 'a'}
+        );
+
+        assertConfigurationEquals(to, transition.apply(from));
+    }
+
     private void assertConfigurationEquals(ComputationConfig expected, ComputationConfig actual) {
         if (expected == null && actual == null)
             return;
